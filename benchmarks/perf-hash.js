3964ecc5cd4815744c7c1ab24f812991ac1bf6de
Daniel Weck <daniel.weck@gmail.com>
2021-03-26 08:19:40 +0000

    removed Twind performance test (cyrb32 hash), added benchmark comments

--- benchmarks/perf-hash.js
+++ benchmarks/perf-hash.js
@@ -2,20 +2,24 @@ const Benchmark = require('benchmark');
 
 const str = `abcdefghijklmnopqrstuvwyxz ABCDEFGHIJKLMNOPQRSTUVWYXZ 0123456789`
 
+// Note: performance comparisons between hash implementations
+// produce susprinsingly different results depending on web browsers.
+// (see https://github.com/cristianbote/goober/pull/271 where tests are made
+// with https://perf.link and https://esbench.com )
+// For example, it turns out that Math.imul() -based hashing is
+// more performant in Firefox and Safari, than in Chrome.
+// That being said, Goober's "improved" hash implementation (see below)
+// is consistently faster in all JS runtimes (including Node).
+
 const suite = new Benchmark.Suite("HASH!");
 suite
-.add('goober original HASH', () => {
+.add('Goober original HASH', () => {
     const c = 'go' + str.split('').reduce((out, s) => (101 * out + s.charCodeAt(0)) >>> 0, 11);
 })
-.add('goober optimized HASH', () => {
+.add('Goober improved HASH', () => {
     let i = 0, l = str.length, out = 11;
     while (i < l) out = (101 * out + str.charCodeAt(i++)) >>> 0
     const c = 'go' + out;
-})
-.add('twind HASH', () => {
-    let i = str.length - 1, out = 9;
-    while (i >= 0) out = Math.imul(out ^ str.charCodeAt(i--), 0x5f356495);
-    const c = 'go' + ((out ^ (out >>> 9)) >>> 0).toString(36);
 })
 .on('start', function (e) {
     console.log('\nStarting', e.currentTarget.name);
